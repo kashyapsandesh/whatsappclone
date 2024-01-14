1de2542553d71ab1dd4e56635350b3eb4c0184a3
Sandesh Ghimire <sandeshghimire202@gmail.com>
2024-01-14 10:27:56 +0545

    initiallized firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/features/app/const/app_const.dart';
 import 'package:whatsappclone/features/app/splash/splash_screen.dart';
 import 'package:whatsappclone/features/app/theme/style.dart';
+import 'package:whatsappclone/firebase_options.dart';
 import 'package:whatsappclone/routes/on_generated_routes.dart';
 import 'main_injection_container.dart' as di;
 
 void main() async {
   await di.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
